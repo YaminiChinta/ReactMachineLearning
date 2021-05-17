486ba6f5bedb767a8c64c7df73248b47fc38399e
YaminiChinta <67141828+YaminiChinta@users.noreply.github.com>
2021-05-17 19:03:28 +0530

    Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ class Footer extends React.Component{
     render(){
         return(
             
-                <div class="totalpage">
+                <div >
                 <div class="mainclass">
                 <div class="footer">
                     <a style={{fontSize: "20px"}} href="https://console.firebase.google.com/u/0/project/dress-2f515/features/all"><strong>See all Firebase features -{">"}</strong></a>
