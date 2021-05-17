45ba13c1bbf31c0ee2874e19eb2eda8f2d5d744d
YaminiChinta <67141828+YaminiChinta@users.noreply.github.com>
2021-05-17 19:04:07 +0530

    updated

--- src/components/Learn.jsx
+++ src/components/Learn.jsx
@@ -6,7 +6,7 @@ class Learn extends React.Component{
     render(){
         return(
             
-                <div class="totalpage">
+                <div>
                 <div class="mainclass">
                 <div class="middlepart">
                         <div class="header"></div>
